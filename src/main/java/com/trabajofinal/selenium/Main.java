e69b05c73cec9c251ab5c63e6f2d40003363e20c
geank <geanmorales@gmail.com>
2026-06-04 20:20:35 -0500

    rename paquete

--- src/main/java/com/trabajofinal/selenium/Main.java
+++ src/main/java/com/trabajofinal/selenium/Main.java
@@ -1,4 +1,4 @@
-package com.multibrowser.lab;
+package com.trabajofinal.selenium;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
